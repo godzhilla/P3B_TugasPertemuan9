d0ce2ac9aec12884f5f6bdb0bf168b2b29ae3a26
godzhilla <ashilanajlasalsabillawd@mail.ugm.ac.id>
2023-10-31 23:28:08 +0700

    First commit

---
+++ app/src/main/java/com/example/tugaspertemuan9/OrderFragment.kt
@@ -0,0 +1,50 @@
+package com.example.tugaspertemuan9
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
+import com.example.tugaspertemuan9.databinding.FragmentOrderBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [OrderFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class OrderFragment : Fragment() {
+    private lateinit var binding: FragmentOrderBinding
+    // TODO: Rename and change types of parameters
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentOrderBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+            spinnerTiket.adapter = ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.jenis_tiket,
+                android.R.layout.simple_spinner_item
+            )
+            btnTicket.setOnClickListener{
+                findNavController().apply {
+                }.navigateUp()
+            }
+        }
+    }
+}
